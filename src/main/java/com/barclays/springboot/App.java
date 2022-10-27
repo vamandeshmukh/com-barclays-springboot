0b792e715f1b9af561f8b4ab256df52cec008f3e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-27 15:18:16 +0530

    spring-boot-basics

--- src/main/java/com/barclays/springboot/App.java
+++ src/main/java/com/barclays/springboot/App.java
@@ -1,13 +1,19 @@
 package com.barclays.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
+		LOG.info("Starting..");
 		SpringApplication.run(App.class, args);
+		LOG.info("Started");
 	}
 
 }
--- src/main/java/com/barclays/springboot/service/EmployeeService.java
+++ src/main/java/com/barclays/springboot/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.barclays.springboot.service;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.slf4j.Logger;
