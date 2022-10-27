966a531405574eb8ab7b4c9e6b09e0346c738dae
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-27 10:31:02 +0530

    spring-boot-basics

--- src/main/java/com/barclays/springboot/controller/EmployeeController.java
+++ src/main/java/com/barclays/springboot/controller/EmployeeController.java
@@ -1,8 +1,26 @@
 package com.barclays.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.barclays.springboot.model.Employee;
+import com.barclays.springboot.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
+
+//	EmployeeService empService = new EmployeeService();
+
+	@Autowired
+	EmployeeService empService;
+
+	// http://localhost:8082/get-emp-by-id/101
+
+	@GetMapping("get-emp-by-id/{eid}")
+	public Employee findEmpById(@PathVariable(name = "eid") int employeeId) {
+		return empService.getEmployeeById(employeeId);
+	}
 
 }
--- src/main/java/com/barclays/springboot/service/EmployeeService.java
+++ src/main/java/com/barclays/springboot/service/EmployeeService.java
@@ -2,7 +2,15 @@ package com.barclays.springboot.service;
 
 import org.springframework.stereotype.Service;
 
+import com.barclays.springboot.model.Employee;
+
 @Service
 public class EmployeeService {
+
+	public Employee getEmployeeById(int employeeId) {
+		Employee emp = new Employee(employeeId, "Sonu", 90000);
+		System.out.println(emp.toString());
+		return emp;
+	}
 
 }
