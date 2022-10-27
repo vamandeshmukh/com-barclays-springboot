93a452d4445bb55a781252816ffc2c0b952e5e08
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-27 16:05:53 +0530

    spring-boot-basics

--- src/main/java/com/barclays/springboot/model/Department.java
+++ src/main/java/com/barclays/springboot/model/Department.java
@@ -14,7 +14,52 @@ public class Department {
 	private String department_name;
 
 	private String city;
-	
-	// List<Emnployee>
+
+	public Department() {
+		super();
+	}
+
+	public Department(String department_name, String city) {
+		super();
+		this.department_name = department_name;
+		this.city = city;
+	}
+
+	public Department(int departmentId, String department_name, String city) {
+		super();
+		this.departmentId = departmentId;
+		this.department_name = department_name;
+		this.city = city;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartment_name() {
+		return department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		this.department_name = department_name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [departmentId=" + departmentId + ", department_name=" + department_name + ", city=" + city
+				+ "]";
+	}
 
 }
