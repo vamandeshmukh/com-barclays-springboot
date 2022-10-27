7c2a281a16414c6f7f9f5bf7f05130d0b9d5ea86
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-27 09:27:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com-barclays-springboot
+ Spring boot RESTful WS demo project for Barclays trainees Oct 2022
---
+++ src/main/java/com/barclays/springboot/ComBarclaysSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBarclaysSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComBarclaysSpringbootApplication.class, args);
+	}
+
+}
